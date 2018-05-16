b3e99426a40fbcae97ce2295583c1024ca3a6bb8
Viktor Bogutskii <vbogutskii@apple.com>
2018-05-15 21:16:42 -0700

    initial offet migrate to sql

--- src/modules/offer/offerModel.js
+++ src/modules/offer/offerModel.js
@@ -1,11 +1,38 @@
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 
-const offerSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  title: { type: String, required: true },
-  catalog: { type: String, required: false },
-  price: { type: Number, required: true },
-  image: { type: Array, required: false },
+// const offerSchema = mongoose.Schema({
+//   _id: mongoose.Schema.Types.ObjectId,
+//   title: { type: String, required: true },
+//   catalog: { type: String, required: false },
+//   price: { type: Number, required: true },
+//   image: { type: Array, required: false },
+// });
+//
+// export default mongoose.model('Offer', offerSchema);
+
+import Sequelize from 'sequelize';
+import sequelize from './../../sql';
+
+const Product = sequelize.define('product', {
+  firstName: {
+    type: Sequelize.STRING,
+  },
+  lastName: {
+    type: Sequelize.STRING,
+  },
 });
 
-export default mongoose.model('Offer', offerSchema);
+export default Product;
+
+// // force: true will drop the table if it already exists
+// Product.sync({ force: false }).then(() =>
+//   // Table created
+//   Product.create({
+//     firstName: 'John 2',
+//     lastName: 'Hancock 2',
+//   }));
+//
+// Product.findAll().then((users) => {
+//   console.log(users);
+// });
+
