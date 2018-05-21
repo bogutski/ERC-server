bf4e1d4a4c81d664e78042654bd06ed9c9e078a3
Viktor Bogutskii <vbogutskii@apple.com>
2018-05-20 19:16:42 -0700

    offer sql WIP

--- src/modules/offer/offerControllers.js
+++ src/modules/offer/offerControllers.js
@@ -1,25 +1,90 @@
 // offer mongoose from 'mongoose';
 // offer _ from 'lodash';
-import parse from 'csv-parse';
-import * as fs from 'fs';
-import Import from './offerModel';
+import Offer from './offerModel';
 import message from './../messages/messages';
 
-export const offerGetAll = (req, res) => {
-  Import.find()
-    .select('-__v')
-    .exec()
-    .then((docs) => {
-      res.status(200)
-        .json(docs);
+export async function offerCreate(req, res) {
+  Offer
+    .findOrCreate({
+      where: {
+        title: req.body.title,
+        price: req.body.price,
+      },
     })
-    .catch((err) => {
-      console.log(err);
-      res.status(500)
-        .json(message.error(err));
+    .spread((user, created) => {
+      const o = user.get({
+        plain: true,
+      });
+      res.status(201)
+        .json(message.success('Offer created', { o, created }));
+      // console.log(created);
     });
-};
+}
 
+// const product = new Product({
+//   _id,
+//   name: req.body.name,
+//   price: req.body.price,
+//   catalog: req.body.catalog,
+//   image: images,
+// });
+
+// Send back product id for redirect to new product after creating
+// const payload = {
+//   productId: _id,
+// };
+//
+// Offer
+//   .save()
+//   .then((result) => {
+//     console.log(result);
+//     res.status(201)
+//       .json(message.success('Product created', payload));
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//     res.status(500)
+//       .json(message.error(err));
+//   });
+// User.create({ username: 'fnord', job: 'omnomnom' })
+//   .then(() => User.findOrCreate({where: {username: 'fnord'}, defaults: {job: 'something else'}}))
+//   .spread((user, created) => {
+//     console.log(user.get({
+//       plain: true
+//     }))
+//     console.log(created)
+//
+//     /*
+//     In this example, findOrCreate returns an array like this:
+//     [ {
+//         username: 'fnord',
+//         job: 'omnomnom',
+//         id: 2,
+//         createdAt: Fri Mar 22 2013 21: 28: 34 GMT + 0100(CET),
+//         updatedAt: Fri Mar 22 2013 21: 28: 34 GMT + 0100(CET)
+//       },
+//       false
+//     ]
+//     The array returned by findOrCreate gets spread into its 2 parts by the "spread" on line 69, and the parts will be passed as 2 arguments to the callback function beginning on line 69, which will then treat them as "user" and "created" in this case. (So "user" will be the object from index 0 of the returned array and "created" will equal "false".)
+//     */
+//   })
+
+// export const offerGetAll = (req, res) => {
+//   Import.find()
+//     .select('-__v')
+//     .exec()
+//     .then((docs) => {
+//       res.status(200)
+//         .json(docs);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500)
+//         .json(message.error(err));
+//     });
+// };
+
+/*
 export async function offerImportDeleteAll(req, res) {
   Import.remove({})
     .exec()
@@ -69,3 +134,6 @@ export async function offerImportJson(req, res) {
 
   afterParse(JSON.parse(json));
 }
+
+
+*/
