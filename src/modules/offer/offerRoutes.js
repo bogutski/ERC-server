7bdfb87319ab91882d5b344d26230f867fa7b085
Viktor Bogutskii <info@siteograf.com>
2018-05-20 19:16:42 -0700

    offer sql WIP

--- src/modules/offer/offerRoutes.js
+++ src/modules/offer/offerRoutes.js
@@ -1,17 +1,19 @@
 import { Router } from 'express';
 
 import {
-  offerImportCsv,
-  offerImportJson,
-  offerImportDeleteAll,
-  offerGetAll,
+  // offerImportCsv,
+  // offerImportJson,
+  // offerImportDeleteAll,
+  // offerGetAll,
+  offerCreate,
 } from './offerControllers';
 
 const router = Router();
 
-router.get('/all', offerGetAll);
-router.delete('/all', offerImportDeleteAll);
-router.post('/import/json', offerImportJson);
-router.post('/import/csv', offerImportCsv);
+// router.get('/all', offerGetAll);
+// router.delete('/all', offerImportDeleteAll);
+router.post('/', offerCreate);
+// router.post('/import/json', offerImportJson);
+// router.post('/import/csv', offerImportCsv);
 
 export default router;
